8de326926389f422e64e81b9c10be812497ef446
Joseph Chiang <joseph.chiang@ignitionapp.com>
2024-10-02 11:34:05 +0000

    Introduce a new onboarding experience for users.
    - Updated manifest to include intro.html.
    - Added click.gif and pin.png for visual guidance.
    - Created intro.html with installation instructions.

--- chrome-extension/src/background/index.ts
+++ chrome-extension/src/background/index.ts
@@ -5,3 +5,9 @@ import './set-newtab-override';
 chrome.action.onClicked.addListener(() => {
   chrome.tabs.create({ url: chrome.runtime.getURL('new-tab/index.html') });
 });
+
+chrome.runtime.onInstalled.addListener(details => {
+  if (details.reason === 'install') {
+    chrome.tabs.create({ url: chrome.runtime.getURL('intro.html') });
+  }
+});
