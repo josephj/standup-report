d1d2bbfecab67a2553490116f869ccaebf742df6
Joseph Chiang <joseph.chiang@ignitionapp.com>
2024-11-27 08:19:49 +0000

    [Operation] Presenting version in the UI
    - Created a new utility function to get the app version.
    - Integrated version retrieval into Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { crx } from '@crxjs/vite-plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
 import manifest from './manifest.json';
+import { version } from './package.json';
 
 export default defineConfig({
-    plugins: [
-        react(),
-        crx({ manifest }),
-        {
-            name: 'markdown-loader',
-            transform(code, id) {
-                if (id.endsWith('.md')) {
-                    return `export default ${JSON.stringify(code)};`;
-                }
-            },
-        },
-    ],
-    resolve: {
-        alias: {
-            '@': '/src',
-        },
+  plugins: [
+    react(),
+    crx({ manifest }),
+    {
+      name: 'markdown-loader',
+      transform(code, id) {
+        if (id.endsWith('.md')) {
+          return `export default ${JSON.stringify(code)};`;
+        }
+      },
     },
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(version),
+  },
 });
